b8fe2c3c849ea8ff01e2ded6b7de6d267b67452e
maxidukov <maxdukovi@gmail.com>
2023-12-07 17:13:03 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 struct point {
   double m_x;
   double m_y;
-  public:
+  //public:
   point(double x, double y) {
     m_x = x;
     m_y = y;
   }
-  double get_m_x() const{ return m_x; }
-  double get_m_y() const{ return m_y; }
+  //double get_m_x() const{ return m_x; }
+  //double get_m_y() const{ return m_y; }
 };
 
 void print_point(const point& point_object);
@@ -26,6 +26,8 @@ int main()
 }
 
 void print_point(const point& point_object) {
-  std::cout << "x:" << point_object.get_m_x() << ", y: "
-    << point_object.get_m_y() << std::endl;
+  //std::cout << "x:" << point_object.get_m_x() << ", y: "
+  //  << point_object.get_m_y() << std::endl;
+  std::cout << "x:" << point_object.m_x << ", y: "
+    << point_object.m_y << std::endl;
 }
